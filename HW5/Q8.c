ef0ce1d6dc51cf27bac36ebcc05bd65eca6c52a2
3RFUNn <efnoskuee@gmail.com>
2022-08-04 14:07:31 +0430

    Q8

---
+++ HW5/Q8.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+    scanf("%d", &n);
+if(n%4==2) {
+    for (i = n / 2; i <= n; i += 2) {
+        for (j = 1; j < n - i; j += 2) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for (i = n; i >= 1; i--) {
+        for (j = i; j < n; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= (i * 2) -1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
+if(n%4==0){
+
+        for(i=n/2; i<n; i+=2)
+        {
+            for(j=0; j<n-i; j+=2)
+            {
+                printf(" ");
+            }
+
+            for(j=1; j<=i; j++)
+            {
+                printf("*");
+            }
+
+            for(j=2; j<=n-i; j++)
+            {
+                printf(" ");
+            }
+
+            for(j=1; j<=i; j++)
+            {
+                printf("*");
+            }
+
+            printf("\n");
+        }
+
+        for(i=n; i>=1; i--)
+        {
+            for(j=i; j<n; j++)
+            {
+                printf(" ");
+            }
+
+            for(j=1; j<=(i*2)-1; j++)
+            {
+                printf("*");
+            }
+
+            printf("\n");
+        }
+}
+    return 0;
+}
